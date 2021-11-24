763dd6b99721053cc45b953ccb9245fcdaf7afba
Guy Sinter <guy.s@tomadox.co.uk>
2021-11-24 14:07:49 +0200

    רקצםהק דפשבק שמ EOL

--- version.php
+++ version.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -19,7 +18,7 @@
  *
  * @package    plagiarism_tomagrade
  * @subpackage plagiarism
- * @copyright  2021 Tomax ltd <roy@tomax.co.il> 
+ * @copyright  2021 Tomax ltd <roy@tomax.co.il>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
 */
 
