6c693336019b3d7af1c866c124ce6462772cfd9e
Guy Sinter <guy.s@tomadox.co.uk>
2021-11-24 14:30:29 +0200

    codechecker fix

--- resetexam.php
+++ resetexam.php
@@ -19,9 +19,9 @@
  *
  * @package    plagiarism_tomagrade
  * @subpackage plagiarism
- * @copyright  2021 Tomax ltd <roy@tomax.co.il> 
+ * @copyright  2021 Tomax ltd <roy@tomax.co.il>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
-*/
+ */
 require_once(dirname(dirname(__FILE__)) . '/../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 require_once($CFG->libdir.'/plagiarismlib.php');
@@ -30,13 +30,12 @@ require_once($CFG->dirroot.'/plagiarism/tomagrade/plagiarism_form.php');
 require_login();
 
 defined('MOODLE_INTERNAL') || die();
-global $DB,$CFG;
-    if ($CFG->version < 2011120100) {
-        $context = get_context_instance(CONTEXT_SYSTEM);
-    } else {
-        $context = context_system::instance();
-    }
-#echo($USER->id."<br>");
+global $DB, $CFG;
+if ($CFG->version < 2011120100) {
+    $context = get_context_instance(CONTEXT_SYSTEM);
+} else {
+    $context = context_system::instance();
+}
 
 if (!isset($_GET['id'])) {
     echo("<script>alert('There was an error, Please contact a system adminstrator.');</script>");
@@ -46,7 +45,7 @@ $cmid = $_GET['id'];
 $data = tomagrade_get_instance_config($cmid);
 
 if ($data->complete > 0) {
-    $DB->execute('UPDATE {plagiarism_tomagrade_config} SET complete = 0 WHERE cm = ?',array($cmid));
+    $DB->execute('UPDATE {plagiarism_tomagrade_config} SET complete = 0 WHERE cm = ?', array($cmid));
     reset_main_grades($cmid);
 }
 echo "<script>window.close();</script>";
