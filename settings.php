6b76070b71164f0350c101e98f45b2ca8026f58c
Guy Sinter <guy.s@tomadox.co.uk>
2021-11-25 10:37:42 +0200

    fix indentations

--- settings.php
+++ settings.php
@@ -31,126 +31,128 @@
 
     require_login();
     admin_externalpage_setup('plagiarismtomagrade');
-    if ($CFG->version < 2011120100) {
-        $context = get_context_instance(CONTEXT_SYSTEM);
-    } else {
-        $context = context_system::instance();
-    }
+if ($CFG->version < 2011120100) {
+    $context = get_context_instance(CONTEXT_SYSTEM);
+} else {
+    $context = context_system::instance();
+}
     require_capability('moodle/site:config', $context, $USER->id, true, "nopermissions");
 
-    //require_once('plagiarism_form.php');
+    // require_once('plagiarism_form.php');
     $mform = new plagiarism_setup_form();
-    //$plagiarismplugin = new plagiarism_plugin_tomagrade();
+    // $plagiarismplugin = new plagiarism_plugin_tomagrade();
     $plagiarismsettings = (array)get_config('plagiarism_tomagrade');
 
-    if ($mform->is_cancelled()) {
-        redirect(new moodle_url('/plagiarism/tomagrade/settings.php'));
-    }
+if ($mform->is_cancelled()) {
+    redirect(new moodle_url('/plagiarism/tomagrade/settings.php'));
+}
 
     echo $OUTPUT->header();
 
-    if (($data = $mform->get_data()) && confirm_sesskey()) {
-        if (!isset($data->tomagrade_use)) {
-            $data->tomagrade_use = 0;
-        }
-        set_config('enabled', $data->tomagrade_use, 'plagiarism_tomagrade');
-
-        $tomagrade_userRolesToDisplayRelatedAssign = "";
-        $tomagrade_userRolesToDisplayRelatedAssign_isfirst = true;
-
-        $tomagrade_userRolesPermissionGradedExam = "";
-        $tomagrade_userRolesPermissionGradedExam_isfirst = true;
-
-        foreach ($data as $field=>$value) {
-            if (isset($plagiarismsettings->$field) && $plagiarismsettings->$field == $value) {
-                //local property copy is equal to submitted property
-                continue; //Setting unchanged
-            }
-
-
-
-
-            if (strpos($field, 'role_') !== false) {
-                $roleId = str_replace("role_","",$field);
-                if (is_numeric($roleId) == false) { continue; }
-
-                if ($tomagrade_userRolesToDisplayRelatedAssign_isfirst) {
-                    $tomagrade_userRolesToDisplayRelatedAssign = $roleId;
-                    $tomagrade_userRolesToDisplayRelatedAssign_isfirst = false;
-                } else {
-                    $tomagrade_userRolesToDisplayRelatedAssign = $tomagrade_userRolesToDisplayRelatedAssign . ",". $roleId;
-                }
-               continue;
-            }
-
-            if (strpos($field, 'rolePermissionGradedExam_') !== false) {
-                $roleId = str_replace("rolePermissionGradedExam_","",$field);
-                if (is_numeric($roleId) == false) { continue; }
-
-                if ($tomagrade_userRolesPermissionGradedExam_isfirst) {
-                    $tomagrade_userRolesPermissionGradedExam = $roleId;
-                    $tomagrade_userRolesPermissionGradedExam_isfirst = false;
-                } else {
-                    $tomagrade_userRolesPermissionGradedExam = $tomagrade_userRolesPermissionGradedExam . ",". $roleId;
-                }
-               continue;
-            }
-
-
-
-            // Save the setting
-            set_config($field, $value, 'plagiarism_tomagrade');
-
-            if (strpos($field, 'tomagrade')===0) {
-                if ($tiiconfigfield = $DB->get_record('config_plugins', array('name'=>$field, 'plugin'=>'plagiarism'))) {
-                    $tiiconfigfield->value = $value;
-                    if (! $DB->update_record('config_plugins', $tiiconfigfield)) {
-                        error("errorupdating");
-                    }
-                } else {
-                    $tiiconfigfield = new stdClass();
-                    $tiiconfigfield->value = $value;
-                    $tiiconfigfield->plugin = 'plagiarism';
-                    $tiiconfigfield->name = $field;
-                    if (! $DB->insert_record('config_plugins', $tiiconfigfield)) {
-                        error("errorinserting");
-                    }
-                }
-            }
-        }
-
-        $plagiarismsettings['tomagrade_userRolesToDisplayRelatedAssign'] = $tomagrade_userRolesToDisplayRelatedAssign;
-        set_config('tomagrade_userRolesToDisplayRelatedAssign', $tomagrade_userRolesToDisplayRelatedAssign, 'plagiarism_tomagrade');
-
-        $plagiarismsettings['tomagrade_userRolesPermissionGradedExam'] = $tomagrade_userRolesPermissionGradedExam;
-        set_config('tomagrade_userRolesPermissionGradedExam', $tomagrade_userRolesPermissionGradedExam, 'plagiarism_tomagrade');
-
-
-        echo $OUTPUT->notification(get_string('savedconfigsuccess', 'plagiarism_tomagrade'), 'notifysuccess');
+if (($data = $mform->get_data()) && confirm_sesskey()) {
+    if (!isset($data->tomagrade_use)) {
+        $data->tomagrade_use = 0;
     }
+    set_config('enabled', $data->tomagrade_use, 'plagiarism_tomagrade');
+
+    $tomagradeuserrolestodisplayrelatedassign = "";
+    $tomagradeuserrolestodisplayrelatedassignisfirst = true;
+
+    $tomagradeuserrolespermissiongradedexam = "";
+    $tomagradeuserrolespermissiongradedexamisfirst = true;
+
+    foreach ($data as $field => $value) {
+        if (isset($plagiarismsettings->$field) && $plagiarismsettings->$field == $value) {
+            // local property copy is equal to submitted property
+            continue; // Setting unchanged
+        }
+
+
+
+
+        if (strpos($field, 'role_') !== false) {
+            $roleid = str_replace("role_", "", $field);
+            if (is_numeric($roleid) == false) { continue;
+            }
+
+            if ($tomagradeuserrolestodisplayrelatedassignisfirst) {
+                $tomagradeuserrolestodisplayrelatedassign = $roleid;
+                $tomagradeuserrolestodisplayrelatedassignisfirst = false;
+            } else {
+                $tomagradeuserrolestodisplayrelatedassign = $tomagradeuserrolestodisplayrelatedassign . ",". $roleid;
+            }
+            continue;
+        }
+
+        if (strpos($field, 'rolePermissionGradedExam_') !== false) {
+            $roleid = str_replace("rolePermissionGradedExam_", "", $field);
+            if (is_numeric($roleid) == false) { continue;
+            }
+
+            if ($tomagradeuserrolespermissiongradedexamisfirst) {
+                $tomagradeuserrolespermissiongradedexam = $roleid;
+                $tomagradeuserrolespermissiongradedexamisfirst = false;
+            } else {
+                $tomagradeuserrolespermissiongradedexam = $tomagradeuserrolespermissiongradedexam . ",". $roleid;
+            }
+            continue;
+        }
+
+
+
+        // Save the setting
+        set_config($field, $value, 'plagiarism_tomagrade');
+
+        if (strpos($field, 'tomagrade') === 0) {
+            if ($tiiconfigfield = $DB->get_record('config_plugins', array('name' => $field, 'plugin' => 'plagiarism'))) {
+                $tiiconfigfield->value = $value;
+                if (! $DB->update_record('config_plugins', $tiiconfigfield)) {
+                    error("errorupdating");
+                }
+            } else {
+                $tiiconfigfield = new stdClass();
+                $tiiconfigfield->value = $value;
+                $tiiconfigfield->plugin = 'plagiarism';
+                $tiiconfigfield->name = $field;
+                if (! $DB->insert_record('config_plugins', $tiiconfigfield)) {
+                    error("errorinserting");
+                }
+            }
+        }
+    }
+
+    $plagiarismsettings['tomagrade_userRolesToDisplayRelatedAssign'] = $tomagradeuserrolestodisplayrelatedassign;
+    set_config('tomagrade_userRolesToDisplayRelatedAssign', $tomagradeuserrolestodisplayrelatedassign, 'plagiarism_tomagrade');
+
+    $plagiarismsettings['tomagrade_userRolesPermissionGradedExam'] = $tomagradeuserrolespermissiongradedexam;
+    set_config('tomagrade_userRolesPermissionGradedExam', $tomagradeuserrolespermissiongradedexam, 'plagiarism_tomagrade');
+
+
+    echo $OUTPUT->notification(get_string('savedconfigsuccess', 'plagiarism_tomagrade'), 'notifysuccess');
+}
 
     // cant do isset($plagiarismsettings['tomagrade_userRolesToDisplayRelatedAssign']) because php 5.4 support
 
-    $settingsKeys = array_keys($plagiarismsettings);
+    $settingskeys = array_keys($plagiarismsettings);
 
-    $rolesArrRelatedUser = array();
-    $rolesArrPermissionGradedExam = array();
-    if (in_array('tomagrade_userRolesToDisplayRelatedAssign',$settingsKeys)) {
-      $rolesArrRelatedUser = explode(",",$plagiarismsettings['tomagrade_userRolesToDisplayRelatedAssign']);
-    }
+    $rolesarrrelateduser = array();
+    $rolesarrpermissiongradedexam = array();
+if (in_array('tomagrade_userRolesToDisplayRelatedAssign', $settingskeys)) {
+    $rolesarrrelateduser = explode(",", $plagiarismsettings['tomagrade_userRolesToDisplayRelatedAssign']);
+}
 
-    if (in_array('tomagrade_userRolesPermissionGradedExam',$settingsKeys)) {
-     $rolesArrPermissionGradedExam = explode(",",$plagiarismsettings['tomagrade_userRolesPermissionGradedExam']);
-    }
+if (in_array('tomagrade_userRolesPermissionGradedExam', $settingskeys)) {
+    $rolesarrpermissiongradedexam = explode(",", $plagiarismsettings['tomagrade_userRolesPermissionGradedExam']);
+}
 
 
-    foreach($rolesArrRelatedUser as $role) {
-        $plagiarismsettings["role_".$role] = true;
-    }
+foreach($rolesarrrelateduser as $role) {
+    $plagiarismsettings["role_".$role] = true;
+}
 
-    foreach($rolesArrPermissionGradedExam as $role) {
-        $plagiarismsettings["rolePermissionGradedExam_".$role] = true;
-    }
+foreach($rolesarrpermissiongradedexam as $role) {
+    $plagiarismsettings["rolePermissionGradedExam_".$role] = true;
+}
 
 
 
