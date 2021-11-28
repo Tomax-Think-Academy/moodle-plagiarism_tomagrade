388bf7f8a44913a99c14f2f66859bac5be962181
Guy Sinter <guy.s@tomadox.co.uk>
2021-11-28 14:25:33 +0200

    fix bat to  fit windows xcopy

--- zip.bat
+++ zip.bat
@@ -1,6 +1,10 @@
-rm TG-moodle.zip
+del  TG-moodle.zip
 mkdir tomagrade
-cp -r ./* ./tomagrade
-rm -rf ./tomagrade/tomagrade
+copy * tomagrade
+Xcopy  /S /I /E classes  tomagrade\classes
+Xcopy  /S /I /E db  tomagrade\db
+Xcopy  /S /I /E lang  tomagrade\lang
+Xcopy  /S /I /E pix  tomagrade\pix
+rmdir tomagrade\tomagrade /s /q
 tar.exe -a -c -f TG-moodle.zip tomagrade
-rm -rf ./tomagrade
+rmdir tomagrade /s /q
