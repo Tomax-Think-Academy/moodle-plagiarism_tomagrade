208807b2ecb16b55f643510a7aab0cc956afce20
Guy Sinter <guy.s@tomadox.co.uk>
2021-12-05 15:27:12 +0200

    updating version

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2021112939;
+$plugin->version = 2021120501;
 $plugin->requires = 2010042803;
 $plugin->component = 'plagiarism_tomagrade';
 $plugin->maturity = MATURITY_STABLE;
