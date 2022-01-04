5c652aad0d232e76ed626f2f632f2448ff36195a
Guy Sinter <guy.s@tomadox.co.uk>
2022-01-04 11:33:02 +0200

    remove zip from this script

--- uploadVersion.bat
+++ uploadVersion.bat
@@ -1,4 +1,4 @@
-zip.bat
+
 aws s3 cp ./TG-moodle.zip s3://public.tomagrade.com/ApplicationSetup/WindowsSetup/TG-moodle.zip
 aws s3api put-object-acl --bucket public.tomagrade.com --key ApplicationSetup/WindowsSetup/TG-moodle.zip --acl public-read
 
