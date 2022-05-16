04de03cad8e4fc4b7ab9dda404b0e33d4d789412
Guy Sinter <guy.s@tomadox.co.uk>
2022-05-16 12:58:15 +0300

    Direct access to raw $_GET/$_POST/$_REQUEST is not allowed

--- resetexam.php
+++ resetexam.php
@@ -29,19 +29,13 @@ require_once($CFG->dirroot.'/plagiarism/tomagrade/lib.php');
 require_once($CFG->dirroot.'/plagiarism/tomagrade/plagiarism_form.php');
 require_login();
 
-defined('MOODLE_INTERNAL') || die();
 global $DB, $CFG;
 if ($CFG->version < 2011120100) {
     $context = get_context_instance(CONTEXT_SYSTEM);
 } else {
     $context = context_system::instance();
 }
-
-if (!isset($_GET['id'])) {
-    echo("<script>alert('There was an error, Please contact a system adminstrator.');</script>");
-    echo("<script>window.close();</script>");
-}
-$cmid = $_GET['id'];
+$cmid = required_param('id', PARAM_INT);
 $data = tomagrade_get_instance_config($cmid);
 
 if ($data->complete > 0) {
